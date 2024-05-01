6f22d362bd67865d81679d3cced05e34b8776e1e
BRENO AMBROSIM LOUZADA <brenoambrosim8@gmail.com>
2024-04-30 21:33:45 -0300

    Mundo3_Nivel2

---
+++ script03_select.sql
@@ -0,0 +1,64 @@
+-- (a)
+SELECT p.*, pf.cpf
+FROM Pessoa p
+INNER JOIN PessoaFisica pf ON p.idPessoa = pf.FK_Pessoa_idPessoa;
+
+-- (b)
+SELECT p.*, pj.cnpj
+FROM Pessoa p
+INNER JOIN PessoaJuridica pj ON p.idPessoa = pj.FK_Pessoa_idPessoa;
+
+-- (c)
+SELECT m.*, p.nome as fornecedor, pr.nome as Produto, m.quantidade, m.precoUnitario, (m.quantidade * m.precoUnitario) as total
+FROM Movimento m
+INNER JOIN Pessoa p ON p.idPessoa = m.FK_Pessoa_idPessoa
+INNER JOIN Produto pr ON pr.idProduto = m.FK_Produto_idProduto
+WHERE m.tipo = 'E';
+
+-- (d)
+SELECT m.*, p.nome as comprador, pr.nome as Produto, m.quantidade, m.precoUnitario, (m.quantidade * m.precoUnitario) as total
+FROM Movimento m
+INNER JOIN Pessoa p ON m.FK_Pessoa_idPessoa = p.idPessoa
+INNER JOIN Produto pr ON m.FK_Produto_idProduto = pr.idProduto
+WHERE m.tipo = 'S';
+
+-- (e)
+SELECT pr.nome, SUM(m.quantidade * m.precoUnitario) as compras
+FROM Movimento m
+INNER JOIN Produto pr ON m.FK_Produto_idProduto = pr.idProduto
+WHERE m.tipo = 'E'
+GROUP BY pr.nome;
+
+-- (f)
+SELECT pr.nome, SUM(m.quantidade * m.precoUnitario) as vendas
+FROM Movimento m
+INNER JOIN Produto pr ON m.FK_Produto_idProduto = pr.idProduto
+WHERE m.tipo = 'S'
+GROUP BY pr.nome;
+
+-- (g)
+SELECT u.*
+FROM Usuario u
+LEFT JOIN Movimento m ON u.idUsuario = m.FK_Usuario_idUsuario AND m.tipo = 'E'
+WHERE m.idMovimento IS NULL;
+
+-- (h)
+SELECT u.loginName, SUM(m.precoUnitario * m.quantidade) as compras
+FROM Movimento m
+INNER JOIN Usuario u ON m.FK_Usuario_idUsuario = u.idUsuario
+WHERE m.tipo = 'E'
+GROUP BY u.loginName;
+
+-- (i)
+SELECT u.loginName, SUM(m.precoUnitario * m.quantidade) as vendas
+FROM Movimento m
+INNER JOIN Usuario u ON m.FK_Usuario_idUsuario = u.idUsuario
+WHERE m.tipo = 'S'
+GROUP BY u.loginName;
+
+-- (j)
+SELECT pr.nome, SUM(m.precoUnitario * m.quantidade) / SUM(m.quantidade) as media
+FROM Movimento m
+INNER JOIN Produto pr ON m.FK_Produto_idProduto = pr.idProduto
+WHERE m.tipo = 'S'
+GROUP BY pr.nome;
